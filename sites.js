9cb5dbda50682d47dc811d3e7fe71823fc3e8e92
Chad Peters PXD2 Soft Dev Group <pxdsquared@gmail.com>
2026-09-21 21:29:40 -0700

    ops hall: one site every box opens

--- sites.js
+++ sites.js
@@ -2,6 +2,7 @@
 (function () {
   var SITES = [
     { name: "Sites", href: "/sites/" },
+    { name: "Ops", href: "/ops/" },
     { name: "HOLD", href: "/hold/" },
     { name: "Hugs", href: "/hugs/" },
     { name: "Edu", href: "/edu/" },
